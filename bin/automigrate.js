73781e2bd77b46c7a5dd0b09b8d529d632e8c1ed
aymen <aymen.sellaouti@gmail.com>
2018-08-24 16:17:48 +0100

    first commit

---
+++ bin/automigrate.js
@@ -0,0 +1,13 @@
+[
+  {
+    email: 'john.doe@ibm.com',
+    createdAt: "Thu Oct 22 2015 17:58:09 GMT-0700 (PDT)",
+    lastModifiedAt: "Thu Oct 22 2015 17:58:09 GMT-0700 (PDT)",
+    id: "562986213ea33440575c6588"
+  },
+  {
+    email: 'jane.doe@ibm.com',
+    createdAt: "Thu Oct 22 2015 17:58:09GMT - 0700(PDT)",
+    lastModifiedAt: "Thu Oct 22 2015 17:58:09 GMT - 0700(PDT)",
+    id: "562986213ea33440575c6587"
+  }]
